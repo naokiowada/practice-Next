80e4250be0b47a22c0ce631856c889c9f375ec9d
naokiowada <naoki.owada.709@gmail.com>
2021-12-05 19:01:25 +0900

    [Add] カウントアプリ完成

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const yuto = () => {
+    const [count, setCount] = useState(0)
+    const incrementCount = () => {
+        setCount(++count)
+    }
+    const decrementCount = () => {
+        setCount(--count)
+    }
+    return (
+        <div style={{backgroundColor: count < 0 ? 'red' : 'white'}}>
+            <div>
+                <p>{count}</p>
+            </div>
+            <button onClick={decrementCount}>-</button>
+            <button onClick={incrementCount}>+</button>
+        </div>
+    )
+}
+export default yuto
